9baff70db6d47df76889a25d62276c8aeb723f52
blartillot <lartillotb@users.noreply.github.com>
2024-04-21 22:27:02 +0200

    init

--- vendor/bo/server/view/core.js
+++ vendor/bo/server/view/core.js
@@ -2,8 +2,7 @@ const renderCore = (context, entity, view) => {
     return `
     <script src="/bo/cli/resources/jquery/jquery-3.6.3.min.js" ></script>
     <script src="/bo/cli/resources/popper/popper.min.js"></script>
-    <script src="/bo/cli/resources/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
-    <script src="/bo/cli/resources/bootstrap-select-1.13.1/dist/js/bootstrap-select.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="/bo/cli/resources/jquery-ui-1.13.2/jquery-ui.js"></script>
     <script src="/bo/cli/resources/jquery.timepicker/jquery.timepicker.js"></script>
     <script src="https://kit.fontawesome.com/a57cef3c40.js" crossorigin="anonymous"></script>
--- vendor/bo/server/view/head.js
+++ vendor/bo/server/view/head.js
@@ -9,8 +9,7 @@ const renderHead = (context, entity, view) => {
       <meta http-equiv="x-ua-compatible" content="ie=edge">
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-      <link rel="stylesheet" href="/bo/cli/resources/bootstrap-4.3.1-dist/css/bootstrap.min.css">
-      <link rel="stylesheet" href="/bo/cli/resources/bootstrap-select-1.13.1/dist/css/bootstrap-select.min.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
       <link rel="stylesheet" href="/bo/cli/resources/jquery-ui-1.13.2/jquery-ui.css">
       <link rel="stylesheet" href="/bo/cli/resources/jquery.timepicker/jquery.timepicker.css">
       <link rel="stylesheet" href="/bo/cli/resources/toastr/build/toastr.min.css" rel="stylesheet" />

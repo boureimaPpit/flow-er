92e6402f99b5d3057db6d611fc7874c1b9f0d5e3
Bruno Lartillot <lartillotb@users.noreply.github.com>
2024-08-20 14:21:14 +0200

    my

--- vendor/my/server/controller/index.js
+++ vendor/my/server/controller/index.js
@@ -4,6 +4,13 @@ const multer = require("multer");
 const { executeService, assert } = require("../../../../core/api-utils")
 const { createDbClient2 } = require("../../../utils/db-client")
 
+const { calendarAction } = require("./calendarAction")
+const { changePasswordAction } = require("./changePasswordAction")
+const { forgotPasswordAction } = require("./forgotPasswordAction")
+const { homeAction } = require("./homeAction")
+const { invoiceAction } = require("./invoiceAction")
+const { mailAction } = require("./mailAction")
+const { profileAction } = require("./profileAction")
 const { protectedFormAction } = require("./protectedFormAction")
 
 const registerMy = async ({ context, config, logger, app }) => {
@@ -12,7 +19,14 @@ const registerMy = async ({ context, config, logger, app }) => {
     const execute = executeService(config, logger)
     const upload = multer()
     app.use(upload.array())
-    app.get(`${config.prefix}protectedForm/:entity/:id`, execute(protectedFormAction, context, db))
+    app.get(`${config.prefix}calendar/:id`, execute(calendarAction, context, db))
+    app.get(`${config.prefix}changePassword/:id`, execute(changePasswordAction, context, db))
+    app.get(`${config.prefix}forgotPassword/:id`, execute(forgotPasswordAction, context, db))
+    app.get(`${config.prefix}home/:id`, execute(homeAction, context, db))
+    app.get(`${config.prefix}invoice/:id`, execute(invoiceAction, context, db))
+    app.get(`${config.prefix}mail/:id`, execute(mailAction, context, db))
+    app.get(`${config.prefix}profile/:id`, execute(profileAction, context, db))
+    app.get(`${config.prefix}protectedForm/:id`, execute(protectedFormAction, context, db))
 }
 
 module.exports = {
